e3c8364cb3955496dd9974a52de8c1224294bb6f
mintaywon <taywonmin@snu.ac.kr>
2023-02-24 16:06:29 +0900

    Modify _convert_equation_label to produce operator_label and operand_label directly
    Add ["PAD] token to operator_encoder and operand_encoder
    
    Fix operator_label to have shape of [B,T] not [B,T,1]

--- datasets/test_dataset.py
+++ datasets/test_dataset.py
@@ -13,6 +13,7 @@ class TestDataset(TestCase):
     # roberta-large, roberta-base, npm, npm-single, AnReu/math_pretrained_roberta have same tokenizer
     models = ["roberta-large", "roberta-base", "facebook/npm", "facebook/npm-single", "witiko/mathberta",
     "AnReu/math_pretrained_bert", "AnReu/math_pretrained_roberta"]
+    # models = ["roberta-base"]
 
     for model in models:
         print("preparing datasets for model: ", model)
@@ -221,19 +222,12 @@ class TestDataset(TestCase):
                 self.assertEqual(batch.attention_mask.shape[0], 5)
                 self.assertEqual(batch.question_mask.shape[0], 5)
                 self.assertEqual(batch.number_mask.shape[0], 5)
-                self.assertEqual(batch.equation_label.shape[0], 5)
                 self.assertEqual(batch.operator_label.shape[0], 5)
                 self.assertEqual(batch.operand_label.shape[0], 5)
                 # check if sequence length is correct
                 self.assertEqual(batch.input_ids.shape[1], batch.attention_mask.shape[1])
                 self.assertEqual(batch.input_ids.shape[1], batch.question_mask.shape[1])
                 self.assertEqual(batch.input_ids.shape[1], batch.number_mask.shape[1])
-                # check if equation label length is correct
-                self.assertEqual(batch.equation_label.shape[1], batch.operator_label.shape[1])
-                self.assertEqual(batch.equation_label.shape[1], batch.operand_label.shape[1])
-                # check if equation label arity is correct
-                self.assertEqual(batch.operator_label.shape[2], 1)
-                self.assertEqual(batch.equation_label.shape[2], batch.operator_label.shape[2] + batch.operand_label.shape[2])
 
     def test_collate_function(self):
         print(f"check models: ", end="")
@@ -243,29 +237,29 @@ class TestDataset(TestCase):
             max_operators_size = dataset.config["max_operators_size"]
             max_operator_operands_size = max(map(max, dataset.config['operator_dict'].values()))
 
-            input_ids = torch.FloatTensor([1, 2, 3, 4, 5, 6]).view(1, 6)
-            attention_mask = torch.FloatTensor([1, 1, 1, 1, 1, 1]).view(1, 6)
-            question_mask = torch.FloatTensor([0, 0, 0, 1, 1, 1]).view(1, 6)
-            number_mask = torch.FloatTensor([0, 0, 1, 0, 0, 0]).view(1, 6)
-            equation_label = torch.FloatTensor([7, 8, 9, 10, 11, 12]).view(1, 2, 3)
-            operator_label = equation_label[:, :, :1]
+            input_ids = torch.Tensor([1, 2, 3, 4, 5, 6]).view(1, 6)
+            attention_mask = torch.Tensor([1, 1, 1, 1, 1, 1]).view(1, 6)
+            question_mask = torch.Tensor([0, 0, 0, 1, 1, 1]).view(1, 6)
+            number_mask = torch.Tensor([0, 0, 1, 0, 0, 0]).view(1, 6)
+            equation_label = torch.Tensor([7, 8, 9, 10, 11, 12]).view(1, 2, 3)
+            operator_label = equation_label[:, :, 0]
             operand_label = equation_label[:, :, 1:]
-            feature1 = Feature(input_ids, attention_mask, question_mask, number_mask, equation_label, operator_label, operand_label)
-            feature2 = Feature(-input_ids, -attention_mask, -question_mask, -number_mask, -equation_label, -operator_label, -operand_label)
+            feature1 = Feature(input_ids, attention_mask, question_mask, number_mask, operator_label, operand_label)
+            feature2 = Feature(-input_ids, -attention_mask, -question_mask, -number_mask, -operator_label, -operand_label)
             collated = dataset.collate_function([feature1, feature2])
 
-            input_ids_ans = torch.Tensor([[1, 2, 3, 4, 5, 6], [-1, -2, -3, -4, -5, -6]]).view(2, 6)
-            attention_mask_ans = torch.Tensor([[1, 1, 1, 1, 1, 1], [-1, -1, -1, -1, -1, -1]]).view(2, 6)
-            question_mask_ans = torch.Tensor([[0, 0, 0, 1, 1, 1], [0, 0, 0, -1, -1, -1]]).view(2, 6)
-            number_mask_ans = torch.Tensor([[0, 0, 1, 0, 0, 0], [0, 0, -1, 0, 0, 0]]).view(2, 6)
-            equation_label_ans = torch.Tensor([[7, 8, 9, 10, 11, 12], [-7, -8, -9, -10, -11, -12]]).view(2, 2, 3)
+            input_ids_ans = torch.LongTensor([[1, 2, 3, 4, 5, 6], [-1, -2, -3, -4, -5, -6]]).view(2, 6)
+            attention_mask_ans = torch.LongTensor([[1, 1, 1, 1, 1, 1], [-1, -1, -1, -1, -1, -1]]).view(2, 6)
+            question_mask_ans = torch.LongTensor([[0, 0, 0, 1, 1, 1], [0, 0, 0, -1, -1, -1]]).view(2, 6)
+            number_mask_ans = torch.LongTensor([[0, 0, 1, 0, 0, 0], [0, 0, -1, 0, 0, 0]]).view(2, 6)
+            equation_label_ans = torch.LongTensor([[7, 8, 9, 10, 11, 12], [-7, -8, -9, -10, -11, -12]]).view(2, 2, 3)
 
             input_ids_ans = F.pad(input_ids_ans, (0, max_input_ids-6), value = dataset.tokenizer.pad_token_id)
             attention_mask_ans = F.pad(attention_mask_ans, (0, max_input_ids-6), value = dataset.tokenizer.pad_token_id)
             question_mask_ans = F.pad(question_mask_ans, (0, max_input_ids-6), value = dataset.tokenizer.pad_token_id)
             number_mask_ans = F.pad(number_mask_ans, (0, max_input_ids-6), value = dataset.tokenizer.pad_token_id)
             equation_label_ans = F.pad(equation_label_ans, (0, max_operator_operands_size - 2, 0, max_operators_size - 2), value = dataset.tokenizer.pad_token_id)
-            operator_label_ans = equation_label_ans[:, :, :1]
+            operator_label_ans = equation_label_ans[:, :, 0]
             operand_label_ans = equation_label_ans[:, :, 1:]
 
             self.assertEqual(collated.input_ids.tolist(), input_ids_ans.tolist())
