2ce15d41eb54ea9810f440fe056199b96e9a9ce1
jisuAn <ajs7270@snu.ac.kr>
2023-02-24 14:06:33 +0900

    Add Wrapper model using pytorch-lightning

---
+++ model/sunny/__init__.py
@@ -0,0 +1,72 @@
+from typing import Any, Union
+
+import torch
+from torch import nn
+
+import pytorch_lightning as pl
+from transformers import AutoModel, AutoConfig
+
+from datasets.dataset import Feature
+from model.sunny.aware_decoder import AwareDecoder
+
+
+class WrapperModel(pl.LightningModule):
+    def __init__(self,
+                 bert_model: str = "roberta-base",
+                 fine_tune: int = 0,
+                 lr: float = 1e-5,
+                 weight_decay: float = 0.0,
+                 warmup_ratio: float = 0.1,
+                 optimizer: str = "adamw"
+                 ):
+        super(WrapperModel, self).__init__()
+
+        # equivalent automatic hyperparameter assignment
+        # assign : self.hparams = {"bert_model": bert_model, "fine_tune": fine_tune ... } : dict[str, Any]
+        self.save_hyperparameters()
+
+        # set metric
+        self.metric = nn.CrossEntropyLoss()
+
+        # set encoder
+        self.encoder = AutoModel.from_pretrained(self.bert_model)
+        self.config = AutoConfig.from_pretrained(self.bert_model)
+
+        # pretrained language model은 fine-tuning하고 싶지 않을 때
+        if not self.fine_tune:
+            for param in self.encoder.parameters():
+                param.requires_grad = False
+
+        # set decoder
+        self.decoder = AwareDecoder(input_hidden_dim=self.config.hidden_size)
+
+    def forward(self, x: Feature):
+        encoder_output = self.encoder(x.input_ids).last_hidden_state
+        operator_logit, operand_logit = self.decoder(encoder_output)
+
+        return operator_logit, operand_logit  # [[B, T, 1], [B, T, A]] : Operator, Operand prediction
+
+    def training_step(self, batch: Feature, batch_idx: int, dataloader_idx: int = 0) -> torch.Tensor:
+        gold_operator_label = batch.operator_label
+        gold_operand_label = batch.operand_label
+
+        operator_logit, operand_logit = self(batch)
+
+        # TODO: Calculate loss
+        loss = torch.Tensor(0)
+
+        return loss
+
+    def configure_optimizers(self) -> tuple[list[str], list[str]]:
+        optims = []
+        schedulers = []
+        if self.hparams.optimizeroptimizer == "adam":
+            optim = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
+        elif self.hparams.optimizer == "adamw":
+            optim = torch.optim.AdamW(self.parameters(), lr=self.hparams.lr)
+        elif self.hparams.optimizer == "sgd":
+            optim = torch.optim.SGD(self.parameters(), lr=self.hparams.lr)
+        else:
+            raise NotImplementedError
+
+        return optims, schedulers
