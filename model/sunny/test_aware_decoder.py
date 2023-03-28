0392aadac4e19bc458bd8767a05d286acd2ed3c1
jisuAn <ajs7270@snu.ac.kr>
2023-03-28 23:25:33 +0900

    Fix test code for aware-decoder

--- model/sunny/test_aware_decoder.py
+++ model/sunny/test_aware_decoder.py
@@ -6,6 +6,7 @@ from model.sunny.aware_decoder import AwareDecoder
 
 class AwareDecoderTest(TestCase):
     input_hidden_dim = 512
+    num_layers = 4
     operator_num = 9  # unk + operator + none
     constant_num = 5
     max_equation = 4
@@ -18,6 +19,7 @@ class AwareDecoderTest(TestCase):
     op_vectors = torch.rand(operator_num, input_hidden_dim * 2)
 
     model = AwareDecoder(input_hidden_dim=input_hidden_dim,
+                         num_layers = num_layers,
                          operator_num=operator_num,
                          const_num=constant_num,
                          operator_vector=op_vectors,
@@ -37,8 +39,26 @@ class AwareDecoderTest(TestCase):
                                     [1, 1, 0, 2, 2, 0, 3, 3, 0, 0, 0]])
         attention_mask = torch.rand(5, 11)
         question_mask = torch.rand(5, 11)
-        self.model(input=input, attention_mask=attention_mask,
-                   question_mask=question_mask, number_mask=number_mask)
+        gold_operators = torch.randint(1, 3, (5, self.max_equation)) # [B, T]
+        gold_operands = torch.randint(1, 3, (5, self.max_equation, self.max_arity)) # [B, T, A]
+        self.model.train()
+        self.model(
+            input=input,
+            attention_mask=attention_mask,
+            question_mask=question_mask,
+            number_mask=number_mask,
+            gold_operators = gold_operators,
+            gold_operands = gold_operands
+        )
+        self.model.eval()
+        self.model(
+            input=input,
+            attention_mask=attention_mask,
+            question_mask=question_mask,
+            number_mask=number_mask,
+            gold_operators=gold_operators,
+            gold_operands=gold_operands
+        )
 
     def test__get_num_vec(self):
         input = torch.rand(5, 11, 512)
